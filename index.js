29e32b95bece2d53e550d7b13dcdc588d03d9728
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-01-27 15:30:04 +0600

    parsing request with node modules

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Dependencies \n
 const http = require("http");
+const { StringDecoder } = require("string_decoder");
+const url = require('url');
 
 // app object - module scaffolding
 const app = {};
@@ -19,8 +21,34 @@ app.createServer = () => {
 
 // Handle Request Response
 app.handleReqRes = (req, res) => {
-  // response handle
-  res.end('Hello Node JS');
+  // handling request
+  // getting the url and parsing it
+  const parseUrl = url.parse(req.url, true);
+  // console.log(parseUrl);
+
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // console.log(trimmedPath);
+
+  const method = req.method.toLowerCase();
+  const queryStringObj = parseUrl.query;
+  const headersObj = req.headers;
+  console.log(headersObj);
+
+  const decoder = new StringDecoder('utf-8');
+  let realData = '';
+
+  req.on('data', (buffer) => {
+    console.log(buffer)
+    realData += decoder.write(buffer);
+  })
+
+  req.on('end', () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handle
+    res.end('Hello Node JS');
+  })
 }
 
 // Start server
