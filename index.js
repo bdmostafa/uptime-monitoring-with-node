77df8e1404e9a4a7718c82410cd9195bfea726c9
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-02-03 18:11:31 +0600

    code refactoring for server and wrokers file

--- index.js
+++ index.js
@@ -1,61 +1,19 @@
-// Dependencies \n
-const http = require("http");
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const data = require('./lib/data');
-const { sendSms } = require("./helpers/notifications");
+// Dependencies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
+// const worker = require('./lib/workers');
 
-// app object - module scaffolding
+// app module scaffolding
 const app = {};
 
-// testing fs ===================
-// data.create(
-//   'test', 
-//   'newFile', 
-//   { 
-//     id: 2, 
-//     name: "Shams", 
-//     profession: "Software Developer" 
-//   },
-//   function(error) {
-//     console.log('Error is ', error);
-//   }
-// );
+app.init = () => {
+  // start server
+  server.init();
 
-// data.read('test', 'newFile', function(err, result) {
-//   console.log(err, result);
-// });
-
-// data.update(
-//   'test', 
-//   'newFile', 
-//   {
-//     id: 999,
-//     course: "Back end",
-//     Institution: "BS-23"
-//   },
-//   function(err) {
-//   console.log(`Error is ${err}`);
-// });
-
-// data.delete('test', 'newFile', function(err) {
-//   console.log(err);
-// });
-
-// testing sendSms ===========
-sendSms('01571776744', 'hello node js', (err) => {
-  console.log(err);
-});
-
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environment.port, () => {
-    console.log(`Listening to port ${environment.port}`);
-  });
+  // start workers
+  workers.init();
 };
 
-app.handleReqRes = handleReqRes;
+app.init();
 
-// Start server
-app.createServer();
+module.exports = app;
